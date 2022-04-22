6bf2ab4ef1a8cd486627e9f64b9e0cfbe3497392
Ludo <ludovic.barusseau@gmail.com>
2022-04-22 12:39:21 +0200

    API change for getProgressSteps() function

--- infer_torchvision_mnasnet_process.py
+++ infer_torchvision_mnasnet_process.py
@@ -76,7 +76,7 @@ class Mnasnet(dataprocess.C2dImageTask):
             for row in f:
                 self.class_names.append(row[:-1])
 
-    def getProgressSteps(self, eltCount=1):
+    def getProgressSteps(self):
         # Function returning the number of progress steps for this process
         # This is handled by the main progress bar of Ikomia application
         return 2
